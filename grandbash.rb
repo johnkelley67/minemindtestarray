a6459588da613c7faf5e7fdfc91806e108972c1e
John Kelley <john.kelley@bridgevalley.edu>
2017-07-17 13:14:59 -0400

    coinchanger

---
+++ grandbash.rb
@@ -0,0 +1,11 @@
+require "minitest"/autorun"
+TestTicketsArray < Minitest::Test
+
+def test_retruns_empty_array_with_no_matches
+	my_number = "1234"
+	bash_numbers = ["1233", "1345", "3455"]
+	assert_equal([], grand_bash(bash_numbers, my_number)
+
+	end
+
+grand_bash
